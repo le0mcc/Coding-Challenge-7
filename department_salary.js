6b3dac194da3e4e72ab7e9ccf8298d607cf39ddf
Leo McClellan <mcclellan2@usf.edu>
2024-10-02 17:10:13 -0400

    Calculate total salary for all departments in the company

--- department_salary.js
+++ department_salary.js
@@ -1,5 +1,4 @@
 // Coding Challenge 7: Company Department Salary Calculation
-
 // Task 1: Create a department structure of employees and their salaries and subordinates
 const company = [
     {
@@ -78,9 +77,9 @@ function calculateEmployeeSalary(employee) {
     // Run a loop to add all subordinates' salaries
     for (let subordinate of employee.subordinates) {
         subTotal += calculateEmployeeSalary(subordinate);
-    }
+    };
     return subTotal;
-}
+};
 
 // Create a function to calculate total salary for all of employees in a department using the former function
 function calculateDepartmentSalary(department) {
@@ -91,4 +90,19 @@ function calculateDepartmentSalary(department) {
     return total;
 };
 
-console.log(`Department total: $${calculateDepartmentSalary(company[0])}`);
+// Call function with department total
+console.log(`Total salaries for department ${company[0].departmentName}: $${calculateDepartmentSalary(company[0])}`);
+
+// Task 3: Create a function to calculate the total salary for all departments
+function calculateCompanySalary(company) {
+    // Initialize total for company
+    let companyTotal = 0;
+    for (department of company) {
+        // Call function for each department in the company and add to total
+        companyTotal += calculateDepartmentSalary(department);
+    };
+    return companyTotal;
+};
+
+// Call function with total company salaries
+console.log(`Total salaries for company: $${calculateCompanySalary(company)}`);
